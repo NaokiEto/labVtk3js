be08392982466235b852b55470ba4e661ecd5568
root <neto@caltech.edu>
2013-07-08 14:33:29 -0400

    Adding the remaining of the files to vtkOBJ directory

---
+++ JSONLoader/vtkOBJ/vtkOBJWriterExample.cxx
@@ -0,0 +1,66 @@
+/*=========================================================================
+ *
+ *  Copyright David Doria 2011 daviddoria@gmail.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+#include <vtkSphereSource.h>
+#include <vtkPolyDataReader.h>
+
+#include "vtkOBJWriter.h"
+
+int main (int argc, char *argv[])
+{
+  vtkSmartPointer<vtkPolyData> input;
+  std::string outputFilename;
+
+  // Verify command line arguments
+  if(argc > 1) // Use the command line arguments
+    {
+    if(argc != 3)
+      {
+      std::cout << "Required arguments: InputFilename.vtp OutputFilename.obj" << std::endl;
+      return EXIT_FAILURE;
+      }
+    vtkSmartPointer<vtkPolyDataReader> reader =
+      vtkSmartPointer<vtkPolyDataReader>::New();
+    reader->SetFileName(argv[1]);
+    reader->Update();
+
+    input = reader->GetOutput();
+
+    outputFilename = argv[2];
+
+    }
+  else
+    {
+    outputFilename = "output.obj";
+    vtkSmartPointer<vtkSphereSource> sphereSource =
+      vtkSmartPointer<vtkSphereSource>::New();
+    sphereSource->Update();
+    input->ShallowCopy(sphereSource->GetOutput());
+    }
+
+  vtkSmartPointer<vtkOBJWriter> writer = 
+      vtkSmartPointer<vtkOBJWriter>::New();
+
+  writer->SetInput(input);
+  writer->SetFileName(outputFilename.c_str());
+  writer->Update();
+
+  return EXIT_SUCCESS;
+}
