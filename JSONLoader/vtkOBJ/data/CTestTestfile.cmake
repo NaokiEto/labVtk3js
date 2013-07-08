82ad1c0054ee2332fc9e47c6020ddee0db95e4a2
root <neto@caltech.edu>
2013-07-08 14:30:12 -0400

    adding a few files to vtkOBJ that mysteriously weren't there

---
+++ JSONLoader/vtkOBJ/data/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /var/www/labvtk/labvtk/vtkOBJWriter
+# Build directory: /var/www/labvtk/labvtk/vtkOBJWriter/data
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(vtkOBJWriterTest "vtkOBJWriterTest")
